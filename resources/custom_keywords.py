19cba5c119953075e6c1ce074deb9b02836dc430
Ducker <mateuszczerewko@gmail.com>
2025-03-14 12:37:35 +0100

    Fix: Ensuring new keywords are visible

--- resources/custom_keywords.py
+++ resources/custom_keywords.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from robot.api.deco import keyword
 from pages.login_page import LoginPage
 
